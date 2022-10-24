9fa9815dc4942f64a9d01327ae4f4038da5da8c4
jvcataquiz <jvcataquiz@gmail.com>
2022-10-24 17:20:18 +0800

    Created a seede to test if it querying all the data form the database

--- ToDoList/src/main/java/com/example/ToDoList/ToDoListApplication.java
+++ ToDoList/src/main/java/com/example/ToDoList/ToDoListApplication.java
@@ -1,13 +1,27 @@
 package com.example.ToDoList;
 
+import com.example.ToDoList.Repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToDoListApplication {
+public class ToDoListApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
 
+	@Autowired
+	private TodoRepository todoRepository;
+	@Override
+	public void run(String... args) throws Exception {
+//		TODO todo1 = new TODO("Wash Dishes");
+//		todoRepository.save(todo1);
+//		TODO todo2 = new TODO("Take a Bath");
+//		todoRepository.save(todo2);
+//		TODO todo3 = new TODO("Go to shopping Store");
+//		todoRepository.save(todo3);
+	}
 }
